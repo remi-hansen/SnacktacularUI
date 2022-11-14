42d0993a5a2aef461cf5f98a09d7e541fb522a6f
Remi Hansen <hansenrd@bc.edu>
2022-11-14 13:29:22 -0500

    Initial Commit

---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//
+//  Created by Remi Pacifico Hansen on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
